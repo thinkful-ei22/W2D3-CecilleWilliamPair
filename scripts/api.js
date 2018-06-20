a24860eab5ce00128dd90865dc1f9afa2c24a34b
Cecille Salazar <cecillersalazar@gmail.com>
2018-06-20 09:27:54 -0700

    Implemented getItems method

--- scripts/api.js
+++ scripts/api.js
@@ -1,22 +1,20 @@
 'use strict';
 
-const API = (function() {  
+const API = (function() {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/CecilleWill';
   const myGetItems = function(callback) {
-    //return callback;
+    $.getJSON(`${BASE_URL}/items`, callback)
     callback('api module works!');
   };
-  
+
   return {
-    BASE_URL, 
+    BASE_URL,
     getItems: myGetItems,
   };
 }());
 
-//API.getItems('api module works!');
-//const callback = 'api module works';
-API.getItems(function(data) {
-  console.log(data);
-});
-  
-console.log(API.BASE_URL);
+// API.getItems(function(data) {
+//   console.log(data);
+// });
+//
+// console.log(API.BASE_URL);
